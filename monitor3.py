3a586e8a09cb419dbf4c2a618cf7c1885acd3947
-gHoTi- <ghoti2007@gmail.com>
2010-07-14 03:00:07 -0400

    use different meaningless char for space fixin'

--- monitor3.py
+++ monitor3.py
@@ -952,7 +952,7 @@ class monitor3(object):
         def search(player):
             f = open('chatlog.txt', 'r')
             chat = []
-            search = search.replace('%20', ' ')
+            search = search.replace('@', ' ')
             for line in f:
                 if re.search(player, line, re.I):
                     chat.append(line)
